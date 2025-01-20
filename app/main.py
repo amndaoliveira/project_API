617df6519c077c881a6e954785b799b6b21316c8
Cleuvya-Vitória <cleuvya@alu.ufc.br>
2025-01-19 22:43:43 -0300

    Alteração de grupo em rotas e modelos

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from sqlmodel import Session, create_engine, SQLModel
 from app.db import initialize_database # isso que vai criar o database
 from app.routes.Usuario import routes as rotas_usuario
+from app.routes.Grupo import routes as rotas_grupo
 
 app = FastAPI()
 app.include_router(rotas_usuario)
+app.include_router(rotas_grupo)
+
 
 @app.get("/")
 def test():
-    return "hello test"
+    return {"message":"hello test"}
 
 if __name__ == "__main__":
     print("oi, to aqui")
